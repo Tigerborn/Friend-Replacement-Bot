0ea6c942af44a84ba5b2207e5bb7a67913c5eb1f
Tigerborn <61367620+Tigerborn@users.noreply.github.com>
2026-07-18 20:21:34 -0500

    Update Homelab.py

--- Homelab.py
+++ Homelab.py
@@ -7,14 +7,18 @@ import subprocess
 
 def backup_container(container):
 
+    print("Starting backup...")
+
     result = subprocess.run(
-        [
-            "/home/caleb/scripts/backup.sh",
-            container
-        ],
+        ["/scripts/backup.sh", container],
         capture_output=True,
-        text=True
+        text=True,
+        timeout=60
     )
+
+    print("Return code:", result.returncode)
+    print("STDOUT:", result.stdout)
+    print("STDERR:", result.stderr)
 
     if result.returncode == 0:
         return f"Successfully backed up {container}."
