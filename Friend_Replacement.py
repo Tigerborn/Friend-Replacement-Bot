304194b97d2e7c4dac096800737abd4b4d14c2bd
Tigerborn <61367620+Tigerborn@users.noreply.github.com>
2026-07-18 23:55:32 -0500

    Update Friend_Replacement.py

--- Friend_Replacement.py
+++ Friend_Replacement.py
@@ -159,7 +159,7 @@ async def backup(interaction: discord.Interaction, container: str):
             ephemeral=True
         )
         return
-
+    await interaction.response.send_message(container)
     await interaction.response.defer()
     result = Homelab.backup_container(container)
     await interaction.followup.send(result)
